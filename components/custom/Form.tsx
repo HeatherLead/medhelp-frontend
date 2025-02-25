ccefc064b190cfb1980199c7ce2e402b5a522c3a
Ayush Bangar <143717997+HeatherLead@users.noreply.github.com>
2025-02-25 19:14:52 +0530

    added InputForm and updated metadata

---
+++ components/custom/Form.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React from "react";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z
+  .object({
+    medicine_name: z.string().optional(),
+    medicine_composition: z.string().optional(),
+    medicine_use: z.string().optional(),
+  })
+  .refine(
+    (data) =>
+      data.medicine_name?.trim() ||
+      data.medicine_composition?.trim() ||
+      data.medicine_use?.trim(),
+    {
+      message: "At least one field must be filled.",
+      path: ["medicine_name"],
+    }
+  );
+const InputForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      medicine_name: "",
+      medicine_composition: "",
+      medicine_use: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="w-96 h-auto border-2 border-black rounded-lg p-5">
+      <h1 className="text-center py-2 text-xl">Enter Medicine Details</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="medicine_name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name of Medicine</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter the name of medicine" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="medicine_composition"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel> Its Composition</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter its chemical composition"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="medicine_use"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Use / Purpose of Medicine</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter its use / purpose " {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default InputForm;
